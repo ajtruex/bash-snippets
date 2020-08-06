054bf060f1ee29c8ffc0cb830f59fea747e64c93
Andrew Truex <andrewtruex@gmail.com>
2020-08-06 07:56:12 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Author: Alexander Epstein https://github.com/alexanderepstein
 currentVersion="1.17.1"
-declare -a tools=(cheat cloudup crypt cryptocurrency currency geo movies newton qrify short siteciphers stocks taste todo weather ytview)
+declare -a tools=(cloudup crypt cryptocurrency currency geo movies newton qrify short siteciphers stocks taste todo weather ytview)
 declare -a extraLinuxTools=(maps)
 declare -a extraDarwinTools
 usedGithubInstallMethod="0"
